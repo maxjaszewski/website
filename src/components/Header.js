ae9a061a05d8d0a0b07aa2482a91f01404e9fdb0
maxjaszewski <maxjasz9@gmail.com>
2023-11-14 20:12:25 +1100

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <div>
+            <h1>
+                Max Jaszewski
+            </h1>
+            <h2>
+                Software Engineer
+            </h2>
+        </div>
+    )
+}
