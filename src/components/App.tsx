1db70240afe9e0d9f7fc302d01aadafedad07df1
maxjaszewski <maxjasz9@gmail.com>
2023-11-27 08:31:20 +1100

    Clean up of website

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,40 +1,56 @@
-import { IndentedWrapper, NavBar, View } from "./App.styles";
-import About from "./About";
-import { Element } from "react-scroll";
-import SectionLink from "./Link";
-import Tetris from "./Tetris";
-import Parser from "./Parser/Parser";
+import { View } from "./App.styles";
+
+import Project from "./Project";
+
+const projects = [
+  {
+    title: "Tetris using Functional Programming and the RxJs library",
+    description: `
+    Tetris implementation using the RxJS library and functional programming principles. This
+    project was produced for the FIT2102 Programming Paradigms unit at Monash University.`,
+    link: "https://maxjaszewski.github.io/FIT2102-Assignment1",
+    image: "/website/assets/tetris.png",
+    source: undefined,
+  },
+  {
+    title: "Personal Website",
+    description: `
+    Personal website using TypeScript and React. Styling in CSS and styled-components.
+    Deployed on Github Pages and built using Github Actions.
+    `,
+    link: "https://maxjaszewski.github.io/website",
+    source: "https:github.com/maxjaszewski/website",
+  },
+];
 
 function App() {
-
   return (
-    <div className="App">
-      <NavBar>
-        <SectionLink toItem="about">About</SectionLink>
-        <SectionLink toItem="project">Projects</SectionLink>
-        <IndentedWrapper>
-          <SectionLink toItem="tetris">Tetris</SectionLink>
-          <SectionLink toItem="parser">
-            Javascript Parser in Haskell
-          </SectionLink>
-        </IndentedWrapper>
-      </NavBar>
-
+    <div style={{ display: "flex", marginTop: "10px" }}>
+      <div style={{ flex: 1 }}></div>
       <View>
-        <Element name="about">
-          <About />
-        </Element>
-        <Element name="project">
-          <h1>Projects</h1>
-          <Element name="tetris">
-            <h2>Tetris</h2>
-            <Tetris />
-          </Element>
-          <Element name="parser">
-            <Parser />
-          </Element>
-        </Element>
+        <h1>Max Jaszewski</h1>
+        <p>
+          CS Student at Monash University. Devops Engineer at National Australia
+          Bank.
+          <br />
+          Interested in Data structures and algorithms.
+        </p>
+        <h2>Projects</h2>
+        <div>
+          {projects.map((project) => {
+            return (
+              <Project
+                title={project.title}
+                description={project.description}
+                link={project.link}
+                source={project.source}
+                image={project.image}
+              />
+            );
+          })}
+        </div>
       </View>
+      <div style={{ flex: 1 }}></div>
     </div>
   );
 }
