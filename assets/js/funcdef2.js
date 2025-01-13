99a42f3438c3cd039d1bee970e3a290e394bd946
maxjaszewski <maxjaszewski@users.noreply.github.com>
2025-01-13 02:35:21 +0000

    deploy: 68c42d070c62e5545e27969477242468b8e1ace7

--- assets/js/funcdef2.js
+++
@@ -1,5 +0,0 @@
-function someNumber2(c, b) {const a = 1;
-
-
-    return ((a + b) + parseInt("123"));}
-  
--- assets/js/tailRecursive2.js
+++
@@ -1,6 +0,0 @@
-function fibonacci(n, pprev, prev) {
-    if (((n < 0) || (n === 0))) { return pprev; }
-    if ((n === 1)) { return prev; }
-    return fibonacci((n - 1), prev, (pprev + prev));
-  }
-  
